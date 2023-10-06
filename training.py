cb329a9826b46ed8a010e02385dc6a2e4f93eae7
HamidBahri <111189283+hamidbahri92@users.noreply.github.com>
2023-10-06 19:29:36 +0330

    training.py

--- training.py
+++ training.py
@@ -1,36 +1,8 @@
-# Import necessary libraries
-from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
-
-def train_model():
-    # Load model and tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-7b-instruct-w4-g64-awq")
-    model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b-instruct-w4-g64-awq")
-
-    # Load training data
-    # Assume data is loaded in a variable called `training_data`
-    # TODO: Load your data
-
-    # Define training arguments
-    training_args = TrainingArguments(
-        output_dir="./results",
-        per_device_train_batch_size=8,
-        num_train_epochs=3,
-        logging_dir="./logs",
-        logging_steps=100,
-        do_train=True,
-        evaluation_strategy="epoch",
-    )
-
-    # Define Trainer and train model
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=training_data,
-        # TODO: Define evaluation dataset if available
-        # eval_dataset=eval_dataset,
-    )
-    trainer.train()
+# main.py
+# Main script for training and evaluating the model
 
 if __name__ == "__main__":
-    train_model()
+    # TODO: Add training and evaluation code
+    pass
+
 
